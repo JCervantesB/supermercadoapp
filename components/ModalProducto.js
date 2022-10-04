67d9e6076b7982756b2a23896da9920054e3092a
Julio Cervantes <65304872+JCervantesB@users.noreply.github.com>
2022-10-04 13:42:08 -0600

    Update ModalProducto.js

--- components/ModalProducto.js
+++ components/ModalProducto.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react" 
 import Image from "next/image"
 import useSuper from "../hooks/useSuper"
-import { formatearDinero } from "../Helpers"
+import { formatearDinero } from "../helpers"
 
 const ModalProducto = () => {
 
